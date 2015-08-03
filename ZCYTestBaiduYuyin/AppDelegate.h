90574838f1a40af1ca2aed822c49521fb91d9a96
zhaochunyang0726 <zhaochunyang0726@163.com>
2015-08-03 17:19:38 +0800

    Initial Commit

---
+++ ZCYTestBaiduYuyin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZCYTestBaiduYuyin
+//
+//  Created by zhaochunyang on 15/8/3.
+//  Copyright (c) 2015年 zhaochunyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
